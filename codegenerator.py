ff011d24572d63abb67e4e51a8399eb0707b7473
201dreamers <201dreamers@gmail.com>
2020-09-27 03:20:33 +0300

    Added generation of code for masm, not only for fasm for 1-lab

--- codegenerator.py
+++ codegenerator.py
@@ -3,9 +3,16 @@ from astbuilder import ASTBuilder
 
 class CodeGenerator:
 
-    def __init__(self, node):
+    def __init__(self, node, asm_type: str = 'fasm'):
         self.return_value = self._walk_tree(node)
+        self.asm_type = asm_type
 
+        if self.asm_type == 'fasm':
+            self._initialize_fasm()
+        elif self.asm_type == 'masm':
+            self._initialize_masm()
+
+    def _initialize_fasm(self):
         self.header = (
             'format ELF64\n'
             'public _start\n'
@@ -23,6 +30,39 @@ class CodeGenerator:
             '\tint 0x80\n'
         )
 
+        self.generated_code = '\n'.join((
+            self.header,
+            self.start_func,
+            self.exit_func
+        ))
+
+    def _initialize_masm(self):
+        self.header = (
+            '.386\n'
+            '.model flat, stdcall\n'
+            '.data\n'
+            '.code\n'
+        )
+
+        self.main_proc = (
+            'main PROC\n'
+            f'\tmov eax, {self.return_value}\n'
+            '\tret\n'
+            'main ENDP\n'
+        )
+
+        self.start_func = (
+            '_start:\n'
+            '\tinvoke main\n'
+            '\tinvoke ExitProcess, 0\n'
+        )
+
+        self.generated_code = '\n'.join((
+            self.header,
+            self.main_proc,
+            self.start_func
+        ))
+
     def _walk_tree(self, node):
         if node.id_ == 'function' and node.name == 'main':
             return self._walk_tree(node.children)
@@ -31,15 +71,10 @@ class CodeGenerator:
         elif node.id_ == 'number':
             return node.value
 
-    def generate(self):
-        with open('source_files/generated.asm', 'w') as asm_file:
-            asm_file.write(
-                '\n'.join((
-                    self.header,
-                    self.start_func,
-                    self.exit_func
-                ))
-            )
+    def write_to_file(self):
+        with open(f'source_files/{self.asm_type}_generated.asm', 'w')\
+                as asm_file:
+            asm_file.write(self.generated_code)
 
 
 if __name__ == "__main__":
@@ -48,5 +83,5 @@ if __name__ == "__main__":
     ast_builder = ASTBuilder(PATH_TO_SOURCE_FILE)
     ast_builder.build_tree()
 
-    cg = CodeGenerator(ast_builder.parsed)
-    cg.generate()
+    cg = CodeGenerator(ast_builder.parsed, asm_type='masm')
+    cg.write_to_file()
